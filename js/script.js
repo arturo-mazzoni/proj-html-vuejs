e466da782fc5cd04a2eba57c4c252cfbba41efcf
arturo <arturomazzoni@gmail.com>
2021-02-16 21:40:47 +0100

    section six

--- js/script.js
+++ js/script.js
@@ -2,12 +2,37 @@ let app = new Vue ({
   el: "#app",
   data: {
     menu: [
+      {
+        testo: "Business",
+        icona: "fas fa-chart-area"
+      },
+      {
+        testo: "Design",
+        icona: "fas fa-palette"
+      },
+      {
+        testo: "Development",
+        icona: "fas fa-cog"
+      },
+      {
+        testo: "LIfestyle",
+        icona: "far fa-smile"
+      },
+      {
+        testo: "Office Productivity",
+        icona: "fas fa-wallet"
+      }
+    ],
+    voto: 4,
+    categories: [
+      "All categories",
       "Business",
       "Design",
       "Development",
+      "IT & Software",
       "Lifestyle",
-      "Office Productivity"
-    ],
-    voto: 4
+      "Marketing",
+      "Office Prodictivity"
+    ]
   }
 });
