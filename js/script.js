4f1b4ce9bd666826ae147d1a232b9a00526ecb9d
arturo <arturomazzoni@gmail.com>
2021-02-16 11:26:30 +0100

    section one

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let app = new Vue ({
+  el: "#app",
+  data: {
+    menu: [
+      "Business",
+      "Design",
+      "Development",
+      "Lifestyle",
+      "Office Productivity"
+    ]
+  }
+});
